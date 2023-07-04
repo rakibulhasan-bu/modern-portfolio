32e4995be21c595f7cebcd84b8c76a4061abca18
Rakibul Hasan <rakibulhasanbu@gmail.com>
2023-07-04 12:17:10 +0600

    project page edited

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -4,7 +4,12 @@ import Layout from '@/components/Layout';
 import Head from 'next/head';
 import Image from 'next/image';
 import Link from 'next/link';
-import img1 from '../../public/images/projects/crypto-screener-cover-image.jpg'
+import athletic from '../../public/images/projects/athletic.png'
+import talent from '../../public/images/projects/talent-spark.png'
+import crappo from '../../public/images/projects/crappo.png'
+import quizHero from '../../public/images/projects/quiz-hero.png'
+import savior from '../../public/images/projects/savior-cuisiner.png'
+import toy from '../../public/images/projects/toy-galaxy.png'
 import TransitionEffect from '@/components/TransitionEffect';
 
 const FeaturedProject = ({ type, title, summary, img, link, github }) => {
@@ -60,27 +65,27 @@ const projects = () => {
             </Head>
             <TransitionEffect />
             <main className='w-full mb-16 flex flex-col items-center justify-center'>
-                <Layout className='pt-16'>
-                    <AnimatedText className={'text-5xl'} text={'A collection of my latest projects!'} />
+                <Layout className='pt-8'>
+                    <AnimatedText className={'!text-6xl'} text={'Discover the digital solutions that make an impact!'} />
                     <div className="flex flex-col lg:grid grid-cols-12 gap-12 mt-12">
                         <div className="col-span-12">
-                            <FeaturedProject github={'/'} link={'/'} img={img1} type={'Feature project'} title={'Athletic Academy'} summary={'A feature-rich Crypto Screener App using React, Tailwind CSS, Context API, React Router and Recharts. It shows detail regarding almost all the cryptocurrency. You can easily convert the price in your local currency.'} />
+                            <FeaturedProject github={'https://github.com/rakibulhasan-bu/athletic-academy-client'} link={'https://athletic-academy.web.app/'} img={athletic} type={'Feature project'} title={'Athletic Academy'} summary={'This website provides user registration and login for secure access. Instructors have profiles displaying their information, while users can browse and enroll in classes with validated seat availability. Authenticated students can manage their classes and payments, while instructors and administrators have separate dashboards for class and user management. The website is designed responsively for optimal viewing on various devices.'} />
                         </div>
                         <div className="col-span-6">
-                            <Project github={'/'} link={'/'} img={img1} type={'Feature project'} title={'Crypto Screener Application'} summary={'A feature-rich Crypto Screener App using React, Tailwind CSS, Context API, React Router and Recharts. It shows detail regarding almost all the cryptocurrency. You can easily convert the price in your local currency.'} />
+                            <Project github={'https://github.com/rakibulhasan-bu/toy-galaxy-client'} link={'https://toy-galaxy-c7132.web.app/'} img={toy} type={'Feature project'} title={'Toy Galaxy'} summary={'Toy Galaxy is a vibrant online marketplace dedicated to toy cars, offering buyers a diverse selection and sellers a platform to showcase their products. With user registration, detailed listings, and categorized toy cars, it aims to create an engaging and safe environment for enthusiasts to explore and purchase their favorite toys.'} />
                         </div>
                         <div className="col-span-6">
-                            <Project github={'/'} link={'/'} img={img1} type={'Feature project'} title={'Crypto Screener Application'} summary={'A feature-rich Crypto Screener App using React, Tailwind CSS, Context API, React Router and Recharts. It shows detail regarding almost all the cryptocurrency. You can easily convert the price in your local currency.'} />
+                            <Project github={'https://github.com/rakibulhasan-bu/Savoir-Cuisiner-client'} link={'https://savoir-cuisiner.web.app/'} img={savior} type={'Feature project'} title={'Savior Cuisiner'} summary={'I have created a comprehensive toy car selling website with sections like About, Contact, Blog, Testimonials, and Statistics. Users can view chefs recipes, log in for authentication, and save favorite recipes. Explore the culinary world of France on our website!'} />
                         </div>
                         <div className="col-span-12">
-                            <FeaturedProject github={'/'} link={'/'} img={img1} type={'Feature project'} title={'Crypto Screener Application'} summary={'A feature-rich Crypto Screener App using React, Tailwind CSS, Context API, React Router and Recharts. It shows detail regarding almost all the cryptocurrency. You can easily convert the price in your local currency.'} />
+                            <FeaturedProject github={'https://github.com/rakibulhasan-bu/cryptocurrency-website'} link={'https://crypto-currency-crappo.netlify.app/'} img={crappo} type={'Feature project'} title={'CRAPPO'} summary={'Discover the exciting world of cryptocurrencies on our website, offering the latest market trends and in-depth analysis. We empower users with knowledge and tools to navigate this revolutionary financial realm, exploring opportunities.'} />
 
                         </div>
                         <div className="col-span-6">
-                            <Project github={'/'} link={'/'} img={img1} type={'Feature project'} title={'Crypto Screener Application'} summary={'A feature-rich Crypto Screener App using React, Tailwind CSS, Context API, React Router and Recharts. It shows detail regarding almost all the cryptocurrency. You can easily convert the price in your local currency.'} />
+                            <Project github={'https://github.com/rakibulhasan-bu/quiz-hero'} link={'https://quiz-hero-phero-rakibulbu.netlify.app/'} img={quizHero} type={'Feature project'} title={'Quiz Hero'} summary={'Introducing our quiz app, an engaging platform that tests your knowledge and offers an interactive learning experience. With a variety of categories and challenging questions, you can enhance your knowledge while having fun. Join us and put your skills to the test in this exciting quiz adventure.'} />
                         </div>
                         <div className="col-span-6">
-                            <Project github={'/'} link={'/'} img={img1} type={'Feature project'} title={'Crypto Screener Application'} summary={'A feature-rich Crypto Screener App using React, Tailwind CSS, Context API, React Router and Recharts. It shows detail regarding almost all the cryptocurrency. You can easily convert the price in your local currency.'} />
+                            <Project github={'/'} link={'https://talent-sparks.netlify.app/'} img={talent} type={'Feature project'} title={'Talent Spark'} summary={'Our job application form allows men to provide personal information and job preferences, including education, work experience, and skills. The job features page enables anyone to browse available job openings, filter by type, location, and experience level. The application status page keeps applicants informed about their job applications, while the area chart displays the number of job applications over time, providing insights into submission trends.'} />
                         </div>
                     </div>
                 </Layout>
